6883d673b1114a33301166325002d16528ff9a96
Andi Stevenli <ndstevenli@outlook.com>
2023-11-20 15:51:23 +0700

    implement geolocator

--- lib/feature/hris/page/hris_page.dart
+++ lib/feature/hris/page/hris_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:internal_tools/feature/hris/widget/hris_web_view.dart';
 import 'package:internal_tools/utils/url.dart';
+// import 'package:web_socket_channel/io.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HrisPage extends StatefulWidget {
   const HrisPage({super.key});
@@ -14,6 +16,45 @@ class HrisPage extends StatefulWidget {
 
 class _HrisPageState extends State<HrisPage> {
   late final WebViewController controller;
+  // final _channel = IOWebSocketChannel.connect(hrisUrl);
+
+  // void _subscribeToWebSocket() {
+  //   _channel.stream.listen(
+  //     (message) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text(message),
+  //         ),
+  //       );
+  //     },
+  //     onError: (error) {
+  //       ScaffoldMessenger.of(context).showSnackBar(
+  //         SnackBar(
+  //           content: Text(error),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
+
+  Future<void> getCurrentPosition() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      showPositionSnackBar(
+          "Longitue : ${position.longitude}\nLatitue : ${position.latitude}");
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  void showPositionSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -21,7 +62,15 @@ class _HrisPageState extends State<HrisPage> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse(hrisUrl));
+    // _subscribeToWebSocket();
+    getCurrentPosition();
   }
+
+  // @override
+  // void dispose() {
+  //   _channel.sink.close();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
